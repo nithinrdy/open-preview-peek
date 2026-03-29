cd3e23a2eca32013629439befe2a334cb5c37a68
Vishnu Nithin Reddy <78612244+nithinrdy@users.noreply.github.com>
2026-03-30 00:18:53 +0530

    simplify preview data retrieval

--- src/modules/preview/hooks/use-data-for-preview.ts
+++ src/modules/preview/hooks/use-data-for-preview.ts
@@ -1,21 +1,42 @@
 import { useEffect, useState } from "react";
 
 import type { PreviewData } from "@src/modules/preview/types";
-import { getDataForPreview } from "../utils/get-data-for-preview";
+import { MessageType } from "@src/types/message";
+
+import { sanitizePreviewData } from "./../utils/sanitize-preview-data";
 
 export const useDataForPreview = () => {
   // string = an error message, undefined = still loading
   const [previewData, setPreviewData] = useState<
     PreviewData | string | undefined
   >(undefined);
-  const refresh = () => {
-    (async () => setPreviewData(await getDataForPreview()))();
+
+  const refreshPreviewData = () => {
+    (async () => {
+      const getDataForPreview = async (): Promise<PreviewData | string> => {
+        try {
+          const scriptResponse = await chrome.runtime.sendMessage({
+            type: MessageType.BackgroundScriptRequest,
+          });
+          const sanitizedDataOrError = sanitizePreviewData(scriptResponse);
+
+          return sanitizedDataOrError
+            ? sanitizedDataOrError
+            : "Failed to fetch preview data from the content script: see extension error logs or the extension's console for more details.";
+        } catch (error) {
+          console.error("Error getting preview data:", error);
+          return error instanceof Error ? error.message : String(error);
+        }
+      };
+
+      setPreviewData(await getDataForPreview());
+    })();
   };
-  useEffect(refresh, []);
+  useEffect(refreshPreviewData, []);
 
   return {
     previewData,
     // Wouldn't exactly need to refresh... ever, so just leaving this commented out.
-    // refresh
+    // refreshPreviewData
   };
 };
